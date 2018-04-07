47ff92e80225cbcc7b3148bbef0ff815f549ee81
Anum Siddiqui <asiddiqui0692@gmail.com>
2018-04-07 03:46:03 +0000

    automatically backed up by learn

--- lib/intro_to_arrays.rb
+++ lib/intro_to_arrays.rb
@@ -21,11 +21,11 @@ def last_element(array)
 end
 
 def first_element_with_array_method(array)
-  array.first
+  array.shift()
 end
 
 def last_element_with_array_method(array)
-  array.last
+  array.pop()
 end
 
 def length_of_array(array)
