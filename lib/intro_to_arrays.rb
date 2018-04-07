67db2c9e6b89e283f6d3c4ec559a519255c32acd
Anum Siddiqui <asiddiqui0692@gmail.com>
2018-04-07 03:37:52 +0000

    automatically backed up by learn

--- lib/intro_to_arrays.rb
+++ lib/intro_to_arrays.rb
@@ -7,3 +7,15 @@ def array_with_two_elements
 ["anum", "dani"]
   return ["anum", "dani"] 
 end
+
+def first_element(array)
+  return array[0]
+end
+
+def third_element(array)
+  array[2]
+end
+
+def last_element(array)
+  array[array.length - 1]
+end
